2cac064b608d9823bb95837c3e5acf39f8392a72
Juliana Spector <julianaspector@gmail.com>
2020-08-21 15:27:20 -0700

    Removed unique keys

--- webhook/setup_webhook/setup_webhook.py
+++ webhook/setup_webhook/setup_webhook.py
@@ -1,18 +1,18 @@
 from boxsdk import JWTAuth
 
 #Path to JWT config
-auth = JWTAuth.from_settings_file('71980991_uastxaq2_config.json')
+auth = JWTAuth.from_settings_file('blah_blah_config.json')
 
 from boxsdk import Client
 
 client = Client(auth)
 
 # set up webhook on root folder and will work with all subfolders
-rootfolder = client.folder(folder_id='110995800136') #Redacted folder
+rootfolder = client.folder(folder_id='109876543210') #Redacted folder
 
 #Set up webhook catcher on Google Sheets
 #https://blog.runscope.com/posts/tutorial-capturing-webhooks-with-google-sheets
-webhook = client.create_webhook(rootfolder, ['FILE.UPLOADED'], 'https://script.google.com/macros/s/AKfycbz38241QkoV3vmOS-7J1j6dmsf_9W9tZH0_zKysnNT3FTAcaacN/exec')
+webhook = client.create_webhook(rootfolder, ['FILE.UPLOADED'], 'https://script.google.com/macros/s/UNIQUE_KEY/exec')
 print('Webhook ID is {0} and the address is {1}'.format(webhook.id, webhook.address))
 
 # delete a webhook
