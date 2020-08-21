d564a19b5b63b64e58a29caefa74159b6babe7cb
Juliana Spector <julianaspector@gmail.com>
2020-08-21 14:52:25 -0700

    Added comments

--- webhook/webhook_capturer/webhook_capturer_to_df.R
+++ webhook/webhook_capturer/webhook_capturer_to_df.R
@@ -1,6 +1,7 @@
 library(data.table) # rbindlist
 library(tidyverse)
-library(googlesheets)
+library(gsheet)
+library(jsonlite)
 
 url <-
   'https://docs.google.com/spreadsheets/d/1Nj4QjYX-NsL8wZoNtUZL3OLGnMmcNhAzBXK9u5x3JBc/edit?usp=sharing'
@@ -9,14 +10,21 @@ file <-
   read.csv(text = gsheet2text(url, format = 'csv'),
            stringsAsFactors = FALSE)
 
-file <- file %>% select(X, X.5) %>% rename(Date = X, Information = X.5)
+# rename columns from Google Sheet to meaningful headers
+file <-
+  file %>% select(X, X.5) %>% rename(Date = X, Information = X.5)
 
+# convert JSON information to R object
 json_data <- lapply(file$Information, fromJSON)
 
+# extract source because information you need is in there
 source <- lapply(json_data, `[[`, c('source'))
 
+# extract URL and file name
 id_name <- lapply(source, `[`, c('id', 'name'))
 
+# bind rows into one dataframe for multiple notifications
 data <- rbindlist(id_name, use.names = TRUE, fill = TRUE)
 
+# paste id to end of full URL
 data$id <- paste0("https://cadwr.app.box.com/file/", data$id)
