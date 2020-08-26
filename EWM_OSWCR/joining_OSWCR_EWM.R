dadb934b6f05c97801ba6f488ddeb75c165b8514
Juliana Spector <julianaspector@gmail.com>
2020-08-26 11:51:12 -0700

    Added join results

---
+++ EWM_OSWCR/joining_OSWCR_EWM.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(data.table)
+
+
+# Read in data from OSWCR and EWM
+ewm_master_site <-
+  fread("~/anaconda3/envs/oracle_env/files/20200824/EWM_MASTER_SITE.csv")
+ewm_station <-
+  fread("~/anaconda3/envs/oracle_env/files/20200824/EWM_STATION.csv")
+viewsql_final <-
+  fread("~/anaconda3/envs/oracle_env/files/20200824/VIEWSQL_FINAL_MAT_VW.csv")
+well_numbers <-
+  fread("~/anaconda3/envs/oracle_env/files/20200824/WELL_NUMBERS_MAT_VW.csv")
+
+ewm_master_site[ewm_master_site == ""] <- NA
+ewm_station[ewm_station == ""] <- NA
+viewsql_final[viewsql_final == ""] <- NA
+well_numbers[well_numbers == ""] <- NA
+
+# Join EWM data by common field EWM_MASTER_SITE_ID
+EWM_join <-
+  full_join(ewm_master_site, ewm_station, by = "EWM_MASTER_SITE_ID")
+
+# Rename columns in EWM data to match OSWCR data
+EWM_join <-
+  EWM_join %>% rename(STATE_WELL_NUMBER = EWM_STATE_WELL_NBR)
+
+
+# Join OSWCR data by common field WCRNUMBER
+OSWCR_join <-
+  full_join(viewsql_final, well_numbers, by = "WCRNUMBER")
+
+# Drop rows with SWN as NA
+OSWCR_join <- OSWCR_join %>% drop_na(STATE_WELL_NUMBER)
+
+# Inner join OSWCR and EWM data by different columns
+swn_join <-
+  inner_join(OSWCR_join, EWM_join, by = "STATE_WELL_NUMBER")
+
+# clean swn_join
+swn_join[swn_join == "NN"] <- NA
+
+# anti-join
+swn_anti_join <- anti_join(EWM_join, OSWCR_join, by="STATE_WELL_NUMBER")
--- cx_oracle/cx_Oracle_example.py
+++ cx_oracle/cx_Oracle_example.py
@@ -13,50 +13,10 @@ SQLcxn = cx_Oracle.connect(connectString, dsn=dsn_tns)
 
 
 #Sample SQL
+query ="""select *
+    FROM EWM_ADM.EWM_STATION
+"""
 
-query ="""SELECT c.GSP_ID,
-
-c.LOCAL_ID,
-
-case when e.subbasin_n is null then e.basin_name else e.subbasin_n end as basin_name,
-
-case when d.MULTI_GSPAR=0 then 'Single Annual Report' else 'Multiple Annual Reports' end as SINGLE_MULTIPLE_AR,
-
-e.basin_subb as subbasin_number,
-
-b.GSPAR_ID,
-
-f.LABEL as REPORT_YEAR
-
- 
-
-FROM
-
-    SGMAADJBASIN.SGMA_GSPAR_GSP b JOIN
-
-    SGMAADJBASIN.SGMA_GSP c
-
-    ON b.GSP_ID = c.GSP_ID JOIN
-
-    SGMAADJBASIN.SGMA_GSPAR_MAIN d
-
-    ON b.GSPAR_ID = d.GSPAR_ID and b.disabled = 0 join
-
-    SGMAADJBASIN.sgma_pub_basin_lookup e
-
-    on d.basin_key = e.basin_key join
-
-    SGMAADJBASIN.SGMA_GSPAR_REPORTING_YEAR f
-
-    ON d.REPORT_YEAR_ID = f.REPORT_YEAR_ID
-
-where d.status = 'SUBMITTED'
-
-    and d.disabled = 0
-
-ORDER BY basin_name,subbasin_number,SINGLE_MULTIPLE_AR,c.GSP_ID,c.LOCAL_ID,b.GSPAR_ID"""
-
- 
 
 #Run query and read result to pandas dataframe
 
@@ -70,5 +30,5 @@ df = pd.read_sql(query, SQLcxn)
 
 #write pandas dataframe to csv file
 
-df.to_csv('~/anaconda3/envs/oracle_env/files/export.csv' , index=False,  mode = 'w', header = True, float_format='%g', encoding='utf-8')
+df.to_csv('~/anaconda3/envs/oracle_env/files/20200824/EWM_STATION.csv' , index=False,  mode = 'w', header = True, float_format='%g', encoding='utf-8')
 
